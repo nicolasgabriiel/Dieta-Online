c8ef7bbeba13f15823d394cb8edd8d00c72cc63a
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-07 22:27:12 -0300

    Criando componente HOME
    Criando componente Home e excluíndo o componente hello world que vem por padrão

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 @click="start">Home</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods:{
+    start(){
+      alert('começamos o projeto porra')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
