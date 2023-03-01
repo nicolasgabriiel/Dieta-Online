13c87b6cecbb0e7f7bb21a3661ab8a8c0ede78ad
Nicolas <nicolasgabriel.developer@gmail.com>
2023-02-28 22:50:06 -0300

    FEAT: Adicionando os dados na tela ao clicar no botão gerar avaliação

--- src/views/FormularioAvaliacaoFisica.vue
+++ src/views/FormularioAvaliacaoFisica.vue
@@ -1,29 +1,34 @@
 <template>
     <div class="box">
-    <HeaderComponent />
-    <div class="prescricao-container">
-        <h1>Avaliação Física</h1>
-        <div class="form-row">
-        <label for="titulo">Nome</label>
-        <input type="text" id="titulo"  placeholder="Digite aqui o seu nome" required>
-        </div>
-        <div class="form-row">
-        <label for="titulo">Idade</label>
-        <input type="text" id="titulo"  placeholder="Digite aqui a sua idade" required>
-        </div>
-        <div class="form-row">
-        <label for="titulo">Peso Atual (Kg)</label>
-        <input type="text" id="titulo"  placeholder="Digite aqui o seu peso" required>
-        </div>
-        <div class="form-row">
-        <label for="horario">Altura Atual (cm)</label>
-        <input type="text" id="altura"  placeholder="Digite aqui a sua altura" required>
-        </div>
-        <div class="form-row">
-        <button type="submit" class="button" >Fazer Avaliação</button>
+        <HeaderComponent />
+        <div class="prescricao-container">
+            <h1>Avaliação Física</h1>
+            <form>
+                <div class="form-row">
+                    <label for="titulo">Nome</label>
+                    <input type="text" id="titulo" placeholder="Digite aqui o seu nome" v-model="Nome" required>
+                </div>
+                <div class="form-row">
+                    <label for="titulo">Idade</label>
+                    <input type="text" id="titulo" placeholder="Digite aqui a sua idade" v-model="Idade" required>
+                </div>
+                <div class="form-row">
+                    <label for="titulo">Peso Atual (Kg)</label>
+                    <input type="text" id="titulo" placeholder="Digite aqui o seu peso"  v-model="Peso" required>
+                </div>
+                <div class="form-row">
+                    <label for="horario">Altura Atual (cm)</label>
+                    <input type="text" id="altura" placeholder="Digite aqui a sua altura" v-model="Altura" required>
+                </div>
+                <div class="form-row"></div>
+                <div class="form-row"></div>
+                <div class="form-row">
+                    <button type="button" class="button" @click="gerarAvaliacao"  >Fazer Avaliação</button>
+                </div>
+            </form>
         </div>
     </div>
-    </div>
+    <div v-html="textoDeAvaliacao"></div>
 </template>
 
 <script>
@@ -31,97 +36,130 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 
 export default {
     name: 'Formulario-Avaliacao-Fisica',
-    components:{
+    components: {
         HeaderComponent
+    },
+    data(){
+        return{
+            Nome: "",
+            Idade: "",
+            Peso: "",
+            Altura: "",
+            textoDeAvaliacao: '',
+        }
+    },
+    methods:{
+        gerarAvaliacao(){
+            console.log(this.Nome, this.Idade, this.Peso, this.Altura)
+                this.textoDeAvaliacao = 
+                `
+                <h1>Paciente ${this.Nome}</h1>
+                <h1>Idade ${this.Idade}</h1>
+                <table>
+                    <tr>
+                        <td>Peso</td>
+                        <td> ${this.Peso}</td>
+                    </tr>
+                    <tr>
+                        <td>Altura</td>
+                        <td> ${this.Altura}</td>
+                    </tr>
+                </table>
+                
+                `
+        }
     }
 }
-       
+
 </script>
 
 <style scoped>
-    .box {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 100%;
-    }
+.box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+}
 
-    .prescricao-container{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 70vw;
-        margin: 3rem auto;
-        box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.3);
-        width: 80vw;
-        padding: 2rem 0;
-    }
+.prescricao-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 70vw;
+    margin: 3rem auto;
+    box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.3);
+    width: 80vw;
+    padding: 2rem 0;
+}
 
-    .form{
+.form {
     display: flex;
     align-items: center;
     flex-direction: column;
     background-color: #f3f3f3;
-    }
+}
 
-    .form-row {
-        margin-bottom: 20px;
-        width: 70vw;
-    }
+.form-row {
+    margin-bottom: 20px;
+    width: 70vw;
+}
 
-    input[type="text"], input[type="time"], textarea {
-        padding: 10px;
-        margin-top: 5px;
-        margin-bottom: 5px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        width: 100%;
-        box-shadow: 2px 2px 2px #ccc;
-    }
+input[type="text"],
+input[type="time"],
+textarea {
+    padding: 10px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    width: 100%;
+    box-shadow: 2px 2px 2px #ccc;
+}
 
-    textarea {
-        height: 100px;
-    }
+textarea {
+    height: 100px;
+}
 
-    .button {
-        padding: 10px 20px;
-        background-color: #AC271E;
-        color: white;
-        cursor: pointer;
-        border: none;
-        border-radius: 5px;
-    }
+.button {
+    padding: 10px 20px;
+    background-color: #AC271E;
+    color: white;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+    transition: .2s;
+}
 
-    .button:hover {
-        background-color: #ee1505;
-    }
+.button:hover {
+    background-color: #ee1505;
+}
 
-    .refeicao {
-        box-shadow: 0px 0px 3px 3px #ccc;
-        padding: 20px;
-        margin-bottom: 20px;
-        border-radius: 5px;
-        width: 350px;
-        text-align: center;
-    }
+.refeicao {
+    box-shadow: 0px 0px 3px 3px #ccc;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    width: 350px;
+    text-align: center;
+}
 
-    .titulo {
-        font-size: 20px;
-        margin-bottom: 10px;
-        font-weight: 700;
-    }
+.titulo {
+    font-size: 20px;
+    margin-bottom: 10px;
+    font-weight: 700;
+}
 
-    .alimentos {
-        margin-bottom: 10px;
-    }
+.alimentos {
+    margin-bottom: 10px;
+}
 
-    .obs {
-        margin-top: 10px;
-        font-style: italic;
-    }
+.obs {
+    margin-top: 10px;
+    font-style: italic;
+}
 </style>
 
 
